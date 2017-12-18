579ac55551ec41fc250be0fa0934923b1748b410
maxk <maxk@corp.doctor.com>
2017-12-18 15:13:31 -0800

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'portfolio',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-35-162-36-87.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/portfolio.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:maxkerschersantelli/Portfolio.git',
+      path: '/home/ubuntu/Portfolio',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
